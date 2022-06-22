655fcb0d3d743a4004e1b7b94c87a59a28ff2cd9
Hua，Ye（华烨） <huay@maxwealthfund.com>
2022-06-22 14:55:05 +0800

    feat: 20220622 14:55

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ignores: [(commit) => commit.includes('init')],
+  ignores: [(commit) => commit.includes('temp')],
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-leading-blank': [2, 'always'],
